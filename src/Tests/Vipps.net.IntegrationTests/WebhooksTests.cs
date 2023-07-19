e254b520e3dc35645395103a6f4198e7e614d933
haakongarfjell <haakon.garfjell@bekk.no>
2023-07-19 16:49:05 +0200

    Added test for webhooks

---
+++ src/Tests/Vipps.net.IntegrationTests/WebhooksTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Vipps.net.Models.Webhooks;
+
+
+namespace Vipps.net.IntegrationTests;
+
+[TestClass]
+public class WebhooksTests
+{
+    [TestMethod]
+    public async Task CreateGetDeleteWebhook()
+    {
+        IVippsApi vippsApi = TestSetup.CreateVippsAPI();
+        
+        // Create webhook
+        RegisterRequest registerRequest = new RegisterRequest()
+        {
+            Url = new Uri("https://webhook.site/ebcd8e27-5031-4dd5-beb7-a2d491cd7641"),
+            Events = new List<string>
+            {
+                WebhookEvent.EPaymentCreated
+            }
+        };
+        var createWebhookResponse = await vippsApi.WebooksService().CreateWebhook(registerRequest);
+        Assert.IsNotNull(createWebhookResponse.Id);
+        Assert.IsNotNull(createWebhookResponse.Secret);
+
+        // Get webhooks
+        var getWebHooksResponse = await vippsApi.WebooksService().GetWebhook();
+        Assert.IsNotNull(getWebHooksResponse);
+        var constainsCreatedWebhook = getWebHooksResponse.Any(webhook =>
+            webhook.Url == registerRequest.Url &&
+            webhook.Events.SequenceEqual(registerRequest.Events) &&
+            webhook.Id == createWebhookResponse.Id);        
+        Assert.IsTrue(constainsCreatedWebhook);
+        
+        
+        // Delete webhooks
+        await vippsApi.WebooksService().DeleteWebhook(createWebhookResponse.Id.ToString());
+    }
+}
